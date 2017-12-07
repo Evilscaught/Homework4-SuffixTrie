b5c3f760c58652d5df9357425d6434a022b32b2b
Scott McKay <Evilscaught@users.noreply.github.com>
2017-12-07 16:29:15 -0700

    Initial Setup

---
+++ README.txt
@@ -0,0 +1,12 @@
+After implementing this substring search tool using the TrieST class, you should have noticed 
+that this isn't a particularly efficient way to go about solving the problem: there's a lot of 
+wasted space in the data structure, and a lot of wasted calculation in the search step. 
+
+- Please describe the approach you've used. What is the run time in terms of the size of S, 
+and in terms of the length of each q?  I recommend testing (doing experiments!) with some 
+large strings, for confirmation. 
+
+- What approaches could you take to improve on this clever idea?
+
+
+ANSWER:
---
+++ SuffixTrie.java
@@ -0,0 +1,24 @@
+/*Author:               Scott McKay
+ *Date of Creation:     Thursday, December 7th, 2017 @4:21 p.m. MST
+ *Purpose of Program:   Search for repetitive strings in a file.
+ *Course:               Data Structures [CSCI-323 Section 00]
+ *Semester:             Autumn Semester 2017
+ *Notes:            
+ *Collaboration:        None
+ */
+
+
+public class SuffixTrie
+{
+    public SuffixTrie()
+    {
+        //Generate a suffix trie that represents all suffixes of of the file S.
+    }
+    
+    public static void main(String[] args) 
+    {
+        //TODO: Accept two arguments:
+        //          1.] Name of the file that contains a long string of S letters (no spaces)
+        //          2.] Name of a file that contains search strings
+    }
+}
